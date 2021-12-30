90bda7673d06fd6e7886074c61e690208f1c8aa3
suzuki-navi <suzuki.navi.dev@gmail.com>
2021-12-30 22:16:23 +0900

    データキー・マスターキーの機能

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
+    <v-col cols="12" sm="12" md="12">
       <v-card>
         <v-card-text>
           <v-textarea
@@ -11,19 +11,44 @@
             dense
             @click="clickData"
           ></v-textarea>
+          <template v-if="encrypted != new_encrypted">
+            <v-textarea
+              label="Encrypted"
+              ref="encrypted2"
+              v-model="encrypted2"
+              outlined
+              dense
+              @click="clickEncrypted"
+            ></v-textarea>
+            <a :href="encrypted_link">link</a>
+          </template>
         </v-card-text>
+      </v-card>
+    </v-col>
+    <v-col cols="12" sm="12" md="12">
+      <v-card>
         <v-card-text>
+          <v-text-field v-model="master_password_0" label="Old Master Password" type="password"></v-text-field>
+          <v-text-field v-model="master_password_1" label="New Master Password" type="password"></v-text-field>
+          <v-text-field v-model="master_password_2" label="New Master Password (Confirm)" type="password"></v-text-field>
+          <v-btn
+            elevation="2"
+            @click="clickRotate"
+          >Rotate</v-btn>
+          <v-alert
+            dense
+            outlined
+            type="error"
+            v-if="errorMessage"
+          >{{errorMessage}}</v-alert>
           <v-textarea
-            label="Encrypted"
-            ref="encrypted2"
-            v-model="encrypted2"
+            label="Data Key Encrypted"
+            ref="data_key_encrypted"
+            v-model="data_key_encrypted"
             outlined
             dense
-            @click="clickEncrypted"
+            @click="clickDataKeyEncrypted"
           ></v-textarea>
-        </v-card-text>
-        <v-card-text>
-          <a :href="'?q=' + encrypted">link</a>
         </v-card-text>
       </v-card>
     </v-col>
@@ -38,6 +63,12 @@ export default {
     return {
       data: "",
       encrypted: "",
+      new_encrypted: "",
+      rotate_counter: 0,
+      master_password_0: "",
+      master_password_1: "",
+      master_password_2: "",
+      errorMessage: "",
     };
   },
   computed: {
@@ -47,17 +78,38 @@ export default {
       },
       set(value) {
         this.data = value;
-        this.encrypted = common.encrypt(this.data, this.$cookies);
+        this.new_encrypted = common.encrypt(this.data, this.$cookies);
       },
     },
     encrypted2: {
       get() {
-        return this.encrypted;
+        return this.new_encrypted;
       },
       set(value) {
-        this.encrypted = value;
-        this.data = common.decrypt(value, this.$cookies);
-      }
+        this.new_encrypted = value;
+        [_, this.data] = common.decrypt(value, this.$cookies);
+      },
+    },
+    encrypted_link: {
+      get() {
+        if (this.new_encrypted.length > 1800) {
+          return "./";
+        } else {
+          return "./?q=" + this.new_encrypted;
+        }
+      },
+    },
+    data_key_encrypted: {
+      get() {
+        this.rotate_counter; // rotate_counterがカウントアップされるたびに再描画されるようにするためのコード
+        const data_key_encrypted = this.$cookies.get("data_key_encrypted");
+        if (data_key_encrypted == undefined)
+          return "";
+        return data_key_encrypted;
+      },
+      set(value) {
+        this.$cookies.set("data_key_encrypted", value)
+      },
     },
   },
   methods: {
@@ -67,6 +119,30 @@ export default {
     clickEncrypted() {
       this.$refs.encrypted2.$el.querySelector("textarea").select();
     },
+    clickDataKeyEncrypted() {
+      this.$refs.data_key_encrypted.$el.querySelector("textarea").select();
+    },
+    clickRotate() {
+      if (this.master_password_1 != this.master_password_2) {
+        this.errorMessage = "New passwords do not match";
+        return;
+      }
+      if (this.master_password_1 == "") {
+        console.log("Empty new password");
+        return;
+      }
+      this.errorMessage = "";
+      common.rotateDataKey(this.master_password_0, this.master_password_1, this.$cookies, (errorMessage) => {
+        this.errorMessage = errorMessage;
+      });
+      if (this.errorMessage == "") {
+        this.master_password_0 = "";
+        this.master_password_1 = "";
+        this.master_password_2 = "";
+        this.rotate_counter++;
+        [this.new_encrypted, this.data] = common.decrypt(this.new_encrypted, this.$cookies);
+      }
+    },
   },
   created() {
     let q = this.$route.query.q;
@@ -74,7 +150,7 @@ export default {
       q = "";
     }
     this.encrypted = q;
-    this.data = common.decrypt(q, this.$cookies);
+    [this.new_encrypted, this.data] = common.decrypt(q, this.$cookies);
   },
 }
 </script>
--- plugins/common.js
+++ plugins/common.js
@@ -3,17 +3,34 @@ import CryptoJS from 'crypto-js';
 export default {
   encrypt(data, cookies) {
     //cookies.get("data_key_encrypted");
-    const data_key = "secret";
-    const encrypted = CryptoJS.AES.encrypt(data, data_key);
-    const encrypted_str = this.base64ToUrl(encrypted.toString());
-    return encrypted_str;
+    const self = this;
+    function encrypt_sub(data, data_key) {
+      const encrypted = CryptoJS.AES.encrypt(data, data_key).toString();
+      const encrypted2 = self.base64ToUrl(encrypted);
+      return encrypted2;
+    }
+    const data_key = this.getFirstDataKey(cookies);
+    return encrypt_sub(data, data_key);
   },
   decrypt(encrypted, cookies) {
-    const data_key = "secret";
-    const encrypted2 = this.base64FromUrl(encrypted);
-    //const len = encrypted2.length;
-    const decrypted = CryptoJS.AES.decrypt(encrypted2, data_key);
-    return decrypted.toString(CryptoJS.enc.Utf8);
+    const self = this;
+    function decrypt_sub(encrypted, data_key) {
+      const encrypted2 = self.base64FromUrl(encrypted);
+      const decrypted = CryptoJS.AES.decrypt(encrypted2, data_key).toString(CryptoJS.enc.Utf8);
+      return decrypted;
+    }
+    let firstFlag = true;
+    for (const data_key of this.getDataKey(cookies)) {
+      const data = decrypt_sub(encrypted, data_key);
+      if (data.length > 0) {
+        if (firstFlag)
+          return [encrypted, data];
+        const encrypted2 = self.encrypt(data, cookies);
+        return [encrypted2, data];
+      }
+      firstFlag = false;
+    }
+    return [encrypted, ""];
   },
   base64ToUrl(str) {
     return this.replaceAll(this.replaceAll(this.replaceAll(str, "+", "-"), "/", "_"), "=", "");
@@ -23,5 +40,52 @@ export default {
   },
   replaceAll(str, searchStr, replaceStr) {
     return str.split(searchStr).join(replaceStr);
+  },
+  getFirstDataKey(cookies) {
+    const data_keys = this.getDataKey(cookies);
+    if (data_keys.length == 0) {
+      return "";
+    }
+    return data_keys[0];
+  },
+  getDataKey(cookies) {
+    const secret_key = cookies.get("secret");
+    if (secret_key == undefined)
+      return [];
+    const data_key_encrypted = cookies.get("data_key_encrypted");
+    if (data_key_encrypted == undefined)
+      return [];
+    const data_key_encoded = CryptoJS.AES.decrypt(data_key_encrypted, secret_key).toString(CryptoJS.enc.Utf8);
+    const data_keys = JSON.parse(data_key_encoded);
+    return data_keys;
+  },
+  calcSecretKey(master_key) {
+    const salt = "slsecrets";
+    const hash = CryptoJS.SHA256(salt + master_key);
+    return CryptoJS.enc.Base64.stringify(hash);
+  },
+  rotateDataKey(old_master_password, new_master_password, cookies, errorHandler) {
+    const old_secret_key = this.calcSecretKey(old_master_password);
+    const old_secret_key_0 = cookies.get("secret");
+    if (old_secret_key_0 != undefined && old_secret_key != old_secret_key_0) {
+      errorHandler("Old password isn't valid");
+      return;
+    }
+    const new_secret_key = this.calcSecretKey(new_master_password);
+
+    const data_keys = this.getDataKey(cookies);
+    data_keys.unshift(this.generatePassword());
+    const new_data_key_encoded = JSON.stringify(data_keys);
+    const new_data_key_encrypted = CryptoJS.AES.encrypt(new_data_key_encoded, new_secret_key).toString();
+    cookies.set("data_key_encrypted", new_data_key_encrypted, {maxAge: 30 * 86400});
+    cookies.set("secret", new_secret_key, {maxAge: 30 * 86400})
+  },
+  generatePassword() {
+    const chs = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz=-#%&";
+    let p = "";
+    for (let i = 0; i < 32; i++) {
+      p += chs.charAt(Math.floor(Math.random() * chs.length));
+    }
+    return p;
   },
 }
