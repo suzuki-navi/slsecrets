5f260e20875fe2d34d87543909b19c2b3934b30b
suzuki-navi <suzuki.navi.dev@gmail.com>
2022-01-31 20:41:57 +0900

    deploy

--- docs/dist/_nuxt/static/1640875323/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fslsecrets\u002Fdist\u002F_nuxt\u002Fstatic\u002F1640875323",layout:"default",error:a,state:{},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fslsecrets\u002Fdist\u002F",assetsPath:"\u002Fslsecrets\u002Fdist\u002F_nuxt\u002F",cdnURL:a}}}}(null));
